c350e8b6e27bd9bd8d8226ef26790a6de9be62f4
Chuck439 <firmankediri123@gmail.com>
2021-07-19 16:47:34 +0000

    add function to clone automatic

---
+++ clone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ================================================
+echo Cloning gcc
+echo ================================================
+
+git clone https://github.com/mvaisakh/gcc-arm64
+git clone https://github.com/mvaisakh/gcc-arm
